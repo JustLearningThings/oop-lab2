938d7e924924fc6faf1d66c8fdb7fab5ba689a0f
JustLearningThings <denismacio@outlook.com>
2022-09-26 18:22:12 +0300

    add Simulation

---
+++ Simulation.cs
@@ -0,0 +1,23 @@
+public class Simulation {
+    int steps { get; init; } = 0;
+    int successes { get; } = 0;
+    int fails { get; } = 0;
+
+    // public Simulation(int nSteps) {
+        
+    // }
+
+    public void run() {
+        for (int i = 0; i < steps; i++) {
+            // ...
+        }
+    }
+
+    public void printStats() {
+        Console.WriteLine("=========== Simulation ==========");
+        Console.WriteLine("Steps done: " + steps);
+        Console.WriteLine("Successes: " + successes);
+        Console.WriteLine("Fails: " + fails);
+        Console.WriteLine("=================================");
+    }
+}
